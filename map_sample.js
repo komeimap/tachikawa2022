7e71a60a0d9f548164b89a16b927c0615d6acc9b
komeimap <komeimap@gmail.com>
2026-05-11 10:32:25 +0900

    Update map_sample.js
    国土地理院淡色地図:

--- map_sample.js
+++ map_sample.js
@@ -5,11 +5,21 @@
         //     attribution: '&copy; ' + mapLink + 'Contributors ',
         //     maxZoom: 19,
         // }).addTo(map);
+
+        //標準地図
+        // mapLink = '<a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>';
+        // L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+        //     attribution: '&copy; ' + mapLink,
+        //     maxZoom: 18,
+        // }).addTo(map);
+
+        //淡色地図
         mapLink = '<a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>';
-        L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+        L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
             attribution: '&copy; ' + mapLink,
             maxZoom: 18,
         }).addTo(map);
+
 
         var marker;
         marker = L.marker([35.696228, 139.419084]).addTo(map)
